1e10ccce34e54ec56d1ddd45489d86382e9b45a9
hedrick28 <109778839+hedrick28@users.noreply.github.com>
2022-08-09 07:39:12 +0800

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+//function moving all zeros to end
+function pushZerosToEnd(moveZeros : any, n : number) {
+    var count : number = 0; // to set count for the new index of the non-zeros
+    for (let i = 0; i < n; i++){
+        if (moveZeros[i].toString() != "0"){
+            // Moving non-zeros to front of the array
+            moveZeros[count++] = moveZeros[i];
+        }
+    }
+    while (count < n){
+        // Moving zeros to the end of the array
+        moveZeros[count++] = 0;
+    }
+}
+
+// Running of Scripts
+var moveZeros = new Array()
+moveZeros = [false,1,0,1,2,0,1,3,"a"];
+var n : number = moveZeros.length;
+pushZerosToEnd(moveZeros, n);
+for (let i = 0; i < n; i++)
+    console.log(moveZeros[i]+ " "); 
